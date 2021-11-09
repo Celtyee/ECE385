01c13f5fdff59689ab3fc6ccbd3dd36708d7a383
Lyon <liyang.19@intl.zju.edu.cn>
2021-11-09 21:32:48 +0800

    accumulator add

--- lab/lab7/main.c
+++ lab/lab7/main.c
@@ -5,15 +5,19 @@
 int main()
 {
 	int i = 0;
-	volatile unsigned int *LED_PIO = (unsigned int*)0x20; //make a pointer to access the PIO block
+	volatile unsigned int *LED_PIO = (unsigned int*)0x40; //make a pointer to access the PIO block
+	volatile unsigned int *SW_IN = (unsigned int*)0x60; // a pointer to access the switch
+	volatile unsigned int *KEY_2 = (unsigned int*) // RESET
+	volatile unsigned int *KEY_3 = (unsigned int*)  // accumulate
+	int accumulator = 0;
+	while(true){
+		if(*KEY_2 == 0)
+			accumulator = 0;
+		else if(*KEY_3 == 0)
+			accumulator += *SW_IN;
+		else
+			accumulator |= 0xFF; //keep the value
+	}	
+	return 1;
 
-	*LED_PIO = 0; //clear all LEDs
-	while ( (1+1) != 3) //infinite loop
-	{
-		for (i = 0; i < 100000; i++); //software delay
-		*LED_PIO |= 0x1; //set LSB
-		for (i = 0; i < 100000; i++); //software delay
-		*LED_PIO &= ~0x1; //clear LSB
-	}
-	return 1; //never gets here
 }
