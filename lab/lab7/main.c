24dc4aeca6b4711af9665f978188d269e1f846fa
Lyon <liyang.19@intl.zju.edu.cn>
2021-11-09 21:41:04 +0800

    lab7 main version 2

--- lab/lab7/main.c
+++ lab/lab7/main.c
@@ -7,14 +7,14 @@ int main()
 	int i = 0;
 	volatile unsigned int *LED_PIO = (unsigned int*)0x40; //make a pointer to access the PIO block
 	volatile unsigned int *SW_IN = (unsigned int*)0x60; // a pointer to access the switch
-	volatile unsigned int *KEY_2 = (unsigned int*) // RESET
-	volatile unsigned int *KEY_3 = (unsigned int*)  // accumulate
+	volatile unsigned int *KEY = (unsigned int*) 0x50;// RESET
+
 	int accumulator = 0;
 	
 	while(true){
-		if(*KEY_2 == 0)
+		if(*KEY_2 == 0x10)
 			accumulator = 0;
-		else if(*KEY_3 == 0)
+		else if(*KEY_3 == 0x01)
 		{
 			accumulator += *SW_IN;
 			if(accumulator > 255)
